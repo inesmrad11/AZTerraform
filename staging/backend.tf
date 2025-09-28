d0af3eef0387bbc9ecb83fe985ea28342b2732dd
inesmrad11 <ines.mrad@esprit.tn>
2025-09-28 03:34:32 +0100

    Add Terraform lock files and initialize backends

--- staging/backend.tf
+++ staging/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "terraform-state-rg-inesmrad"
+    resource_group_name  = "terraform-state-rg-ines"
     storage_account_name = "tfstagebackend2025ines"
     container_name      = "tfstate"
     key                 = "stage.tfstate"
